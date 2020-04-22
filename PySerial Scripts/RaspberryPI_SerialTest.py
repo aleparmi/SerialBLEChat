da58cf8d6b2863ac41bff40c3d6cbfa3734ddbd9
baayal <theodor.fischer@smail.th-koeln.de>
2020-04-22 16:52:29 +0200

    added raspberry pi serial test python file.

---
+++ PySerial Scripts/RaspberryPI_SerialTest.py
@@ -0,0 +1,32 @@
+import serial
+import threading
+
+
+ser = serial.Serial('/dev/rfcomm0')
+
+def input_func():
+    while True:
+        t = input() + '\n'
+        ser.write(t.encode())
+
+
+def main():
+
+    if ser.isOpen() is False:
+        print('Serial port is not open. Quitting...')
+        quit()
+    
+    print('Connected to Serial port.')
+    
+    x = threading.Thread(target=input_func)
+    x.start()
+
+    try:
+        while True:
+            print(str(ser.readline()))
+
+    except KeyboardInterrupt:
+        print('Ending Program.') 
+
+if __name__ == '__main__':
+    main()
