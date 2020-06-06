fceb5f6b21a43a68b4be62c8b5d71b8ca6084400
aleparmi <alessandroparmigiani@outlook.com>
2020-06-06 20:55:59 +0200

    Exclude serial folder

--- Client/ClientC++/serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Andrea/Google Drive/Hochschule/NGN/Project/Project_local/serial
-# Build directory: C:/Users/Andrea/Google Drive/Hochschule/NGN/Project/Project_local/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Client/ClientC++/serial/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "C:/Users/Andrea/Google Drive/Hochschule/NGN/Project/Project_local/serial/include".split(';') if "C:/Users/Andrea/Google Drive/Hochschule/NGN/Project/Project_local/serial/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lserial".split(';') if "-lserial" != "" else []
-PROJECT_NAME = "serial"
-PROJECT_SPACE_DIR = "C:/Users/Andrea/Google Drive/Hochschule/NGN/Project/Project_local/serial/devel"
-PROJECT_VERSION = "1.2.1"
